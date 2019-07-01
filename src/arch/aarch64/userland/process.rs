b90ec3a35144064f9889084fb7c9e963730cc52d
dan pittman <dan@auxon.io>
2019-07-01 10:12:12 -0700

    this does not work

--- src/arch/aarch64/userland/process.rs
+++ src/arch/aarch64/userland/process.rs
@@ -1,5 +1,5 @@
 use core::cmp;
-use core::mem::{self, size_of};
+use core::mem;
 use core::ptr;
 
 use selfe_sys::*;
@@ -13,135 +13,84 @@ pub(crate) unsafe fn setup_initial_stack_and_regs(
     param_size: usize,
     stack_top: *mut usize,
 ) -> (seL4_UserContext, usize) {
-    let word_size = size_of::<usize>();
-
-    // The 'tail' is the part of the parameter that doesn't fit in the
-    // word-aligned part.
-    let tail_size = param_size % word_size;
-
-    // The parameter must be zero-padded, at the end, to a word boundary
-    let padding_size = if tail_size == 0 {
-        0
-    } else {
-        word_size - tail_size
-    };
-    let padded_param_size = param_size + padding_size;
-
-    let param_fits_in_regs = padded_param_size <= (8 * word_size);
-
+    let word_size = mem::size_of::<usize>();
+    let alignment_gap = param_size % word_size;
+    let padded_param_size = param_size + alignment_gap;
     let mut regs: seL4_UserContext = mem::zeroed();
 
-    // The cursor pointer to traverse the parameter data word one word at a
-    // time
-    if param_fits_in_regs {
-        let mut p = param;
-
-        // This is the pointer to the start of the tail.
-        let tail = (p as *const u8).add(param_size).sub(tail_size);
-
-        // Compute the tail word ahead of time, for easy use below.
-        let mut tail_word = 0usize;
-        if tail_size >= 1 {
-            tail_word |= *tail.add(0) as usize;
+    if alignment_gap != 0 {
+        let mut ptr = param as *mut u8;
+        ptr = ptr.add(param_size);
+        for _ in 0..alignment_gap {
+            ptr::write_volatile(ptr, 0_u8);
+            ptr = ptr.add(1);
         }
-
-        if tail_size >= 2 {
-            tail_word |= (*tail.add(1) as usize) << 8;
-        }
-
-        if tail_size >= 3 {
-            tail_word |= (*tail.add(2) as usize) << 16;
-        }
-
-        if tail_size >= 4 {
-            tail_word |= (*tail.add(3) as usize) << 24;
-        }
-
-        if tail_size >= 5 {
-            tail_word |= (*tail.add(4) as usize) << 32;
-        }
-
-        if tail_size >= 6 {
-            tail_word |= (*tail.add(5) as usize) << 40;
-        }
-
-        if tail_size >= 7 {
-            tail_word |= (*tail.add(6) as usize) << 48;
-        }
-
-        // Fill up x0 - r7 with the first 8 words.
-
-        if p < tail as *const usize {
-            // If we've got a whole word worth of data, put the whole thing in
-            // the register.
-            regs.x0 = *p;
-            p = p.add(1);
-        } else {
-            // If not, store the pre-computed tail word here and be done.
-            regs.x0 = tail_word;
-            return (regs, 0);
-        }
-
-        if p < tail as *const usize {
-            regs.x1 = *p;
-            p = p.add(1);
-        } else {
-            regs.x1 = tail_word;
-            return (regs, 0);
-        }
-
-        if p < tail as *const usize {
-            regs.x2 = *p;
-            p = p.add(1);
-        } else {
-            regs.x2 = tail_word;
-            return (regs, 0);
-        }
-
-        if p < tail as *const usize {
-            regs.x3 = *p;
-            p = p.add(1);
-        } else {
-            regs.x3 = tail_word;
-            return (regs, 0);
-        }
-
-        if p < tail as *const usize {
-            regs.x4 = *p;
-            p = p.add(1);
-        } else {
-            regs.x4 = tail_word;
-            return (regs, 0);
-        }
-
-        if p < tail as *const usize {
-            regs.x5 = *p;
-            p = p.add(1);
-        } else {
-            regs.x5 = tail_word;
-            return (regs, 0);
-        }
-
-        if p < tail as *const usize {
-            regs.x6 = *p;
-            p = p.add(1);
-        } else {
-            regs.x6 = tail_word;
-            return (regs, 0);
-        }
-
-        if p < tail as *const usize {
-            regs.x7 = *p;
-        } else {
-            regs.x7 = tail_word;
-            return (regs, 0);
-        }
-    } else {
-        let sp = (stack_top as *mut u8).sub(padded_param_size);
-        ptr::copy_nonoverlapping(param as *const u8, sp, padded_param_size);
     }
 
-    (regs, padded_param_size)
+    if padded_param_size >= word_size * 8 {
+        let mut ptr = param;
+        regs.x0 = *ptr;
+        ptr = ptr.add(1);
+
+        if ptr as usize != padded_param_size {
+            regs.x1 = *ptr;
+            ptr = ptr.add(1);
+        } else {
+            return (regs, padded_param_size);
+        }
+
+        if ptr as usize != padded_param_size {
+            regs.x2 = *ptr;
+            ptr = ptr.add(1);
+        } else {
+            return (regs, padded_param_size);
+        }
+
+        if ptr as usize != padded_param_size {
+            regs.x3 = *ptr;
+            ptr = ptr.add(1);
+        } else {
+            return (regs, padded_param_size);
+        }
+
+        if ptr as usize != padded_param_size {
+            regs.x4 = *ptr;
+            ptr = ptr.add(1);
+        } else {
+            return (regs, padded_param_size);
+        }
+
+        if ptr as usize != padded_param_size {
+            regs.x5 = *ptr;
+            ptr = ptr.add(1);
+        } else {
+            return (regs, padded_param_size);
+        }
+
+        if ptr as usize != padded_param_size {
+            regs.x6 = *ptr;
+            ptr = ptr.add(1);
+        } else {
+            return (regs, padded_param_size);
+        }
+
+        if ptr as usize != padded_param_size {
+            regs.x7 = *ptr;
+            ptr = ptr.add(1);
+        } else {
+            return (regs, padded_param_size);
+        }
+        return (regs, padded_param_size);
+    } else {
+        let mut ptr = param as *const u8;
+        let mut stack_ptr = stack_top as *mut u8;
+        for offset in 0..padded_param_size {
+            ptr::write_volatile(stack_ptr, *ptr);
+            ptr = ptr.add(1);
+            stack_ptr = stack_ptr.sub(1);
+        }
+        return (regs, padded_param_size);
+    }
 }
 
 pub(crate) fn set_thread_link_register(
@@ -150,6 +99,7 @@ pub(crate) fn set_thread_link_register(
 ) {
     registers.x30 = (post_return_fn as *const fn() -> !) as usize;
 }
+
 #[doc(hidden)]
 #[allow(dead_code)]
 #[cfg(feature = "test_support")]
