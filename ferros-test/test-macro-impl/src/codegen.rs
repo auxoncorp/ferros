e253afe22ad6aa24e891aaaec40c79ad7eed21ff
dan pittman <dan@auxon.io>
2019-07-02 13:35:43 -0700

    whoops! codegenning debug_println!'s

--- ferros-test/test-macro-impl/src/codegen.rs
+++ ferros-test/test-macro-impl/src/codegen.rs
@@ -99,10 +99,7 @@ fn call_fn_under_test(
         UserTestFnOutput::Result => parse_quote! {{
             match #call {
                 Ok(_) => ferros::test_support::TestOutcome::Success,
-                Err(e) => {
-                    ferros::debug_println!("Test failed:\n {:#?}\n", e);
-                    ferros::test_support::TestOutcome::Failure
-                }
+                Err(_) => ferros::test_support::TestOutcome::Failure,
             }
         }},
     }
