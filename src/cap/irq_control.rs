46ae6955dea8889c26838bff55c5d3ec3ac94258
Zachary Pierce <zack@auxon.io>
2019-07-09 13:30:35 -0700

    Nominal implementation of IRQControl splitting

--- src/cap/irq_control.rs
+++ src/cap/irq_control.rs
@@ -5,9 +5,10 @@ use typenum::*;
 use selfe_sys::*;
 
 use crate::cap::{
-    irq_handler, irq_state, CNodeRole, CNodeSlot, Cap, CapType, IRQHandler, LocalCap,
+    irq_handler, irq_state, CNodeRole, CNodeSlot, Cap, CapType, IRQHandler, LocalCNode, LocalCap,
 };
 use crate::error::{ErrorExt, SeL4Error};
+use crate::userland::CapRights;
 
 pub type MaxIRQCount = U1024;
 
@@ -21,9 +22,9 @@ impl CapType for IRQControl {}
 #[derive(Debug)]
 pub enum IRQError {
     /// The IRQ has already been claimed
-    UnavailableIRQ,
+    UnavailableIRQ(u16),
     /// The IRQ requested is not in the supported range of possible IRQs
-    OutOfRangeIRQ,
+    OutOfRangeIRQ(u16),
     /// The kernel has a problem with how IRQ management is proceeding
     SeL4Error(SeL4Error),
 }
@@ -58,7 +59,7 @@ impl LocalCap<IRQControl> {
         irq: u16,
     ) -> Result<Cap<irq_handler::weak::WIRQHandler<irq_state::Unset>, DestRole>, IRQError> {
         if irq >= MaxIRQCount::U16 {
-            return Err(IRQError::OutOfRangeIRQ);
+            return Err(IRQError::OutOfRangeIRQ(irq));
         }
         let destination_relative_cptr = self.internal_create_handler(dest_slot, irq)?;
         Ok(Cap {
@@ -79,7 +80,7 @@ impl LocalCap<IRQControl> {
         let (dest_cptr, dest_offset, _) = dest_slot.elim();
 
         if self.cap_data.known_handled[usize::from(irq)] {
-            return Err(IRQError::UnavailableIRQ);
+            return Err(IRQError::UnavailableIRQ(irq));
         }
         unsafe {
             seL4_IRQControl_Get(
@@ -95,5 +96,37 @@ impl LocalCap<IRQControl> {
 
         self.cap_data.known_handled[usize::from(irq)] = true;
         Ok(dest_offset)
+    }
+
+    pub fn request_split<DestRole: CNodeRole>(
+        &mut self,
+        src_cnode: &LocalCap<LocalCNode>,
+        dest_slot: CNodeSlot<DestRole>,
+        requested_irqs: [bool; MaxIRQCount::USIZE],
+    ) -> Result<Cap<IRQControl, DestRole>, IRQError> {
+        // First pass to detect already-handled IRQs and reject the request without mutation
+        for (irq, (is_requested, is_already_handled)) in (0..MaxIRQCount::U16).zip(
+            requested_irqs
+                .iter()
+                .zip(self.cap_data.known_handled.iter()),
+        ) {
+            if *is_requested && *is_already_handled {
+                return Err(IRQError::UnavailableIRQ(irq));
+            }
+        }
+        let dest_offset = self.unchecked_copy(src_cnode, dest_slot, CapRights::RWG)?;
+        for (is_requested, known_handled_state) in requested_irqs
+            .iter()
+            .zip(self.cap_data.known_handled.iter_mut())
+        {
+            *known_handled_state = *known_handled_state | *is_requested;
+        }
+        Ok(Cap {
+            cptr: dest_offset,
+            cap_data: IRQControl {
+                known_handled: requested_irqs,
+            },
+            _role: PhantomData,
+        })
     }
 }
