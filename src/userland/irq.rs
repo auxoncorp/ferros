93d4f41ca4eb831a0d6bbcbf6d57a71a19b3478d
Russell Mull <russell@auxon.io>
2019-07-19 11:37:31 -0700

    Add WIRQHandlerCollection.best_effort_new

--- src/userland/irq.rs
+++ src/userland/irq.rs
@@ -52,6 +52,46 @@ impl<Role: CNodeRole> WIRQHandlerCollection<Role> {
         Ok(WIRQHandlerCollection { handlers })
     }
 
+    /// Build a collection around all the handlers we can actually acquire; if
+    /// the kernel (or somebody else) already has one of them, just leave it
+    /// out.
+    pub fn best_effort_new(
+        irq_control: &mut LocalCap<IRQControl>,
+        dest_slots: &mut LocalCap<WCNodeSlotsData<Role>>,
+        requested_irqs: [bool; MaxIRQCount::USIZE],
+    ) -> Result<Self, IRQCollectionError> {
+        let requested_count = requested_irqs.iter().filter(|r| **r).count();
+        if dest_slots.cap_data.size < requested_count {
+            return Err(IRQCollectionError::NotEnoughSlots);
+        }
+        // First pass to detect requested-but-unavailable IRQs and reject the request without mutation
+        for (irq, (is_requested, is_available)) in (0..MaxIRQCount::U16).zip(
+            requested_irqs
+                .iter()
+                .zip(irq_control.cap_data.available.iter()),
+        ) {
+            if *is_requested && !*is_available {
+                return Err(IRQCollectionError::IRQError(IRQError::UnavailableIRQ(irq)));
+            }
+        }
+        let mut handlers = ArrayVec::new();
+        for (irq, is_requested) in (0..MaxIRQCount::U16).zip(requested_irqs.iter()) {
+            // The source instance of IRQControl should treat the IRQs split off into the other
+            // instance as if they were unavailable.
+            if *is_requested {
+                let slot = dest_slots
+                    .alloc_strong()
+                    .map_err(|_| IRQCollectionError::NotEnoughSlots)?;
+                match irq_control.create_weak_handler(slot, irq) {
+                    Ok(h) => handlers.push(h),
+                    // Swallow the error
+                    Err(e) => (),
+                }
+            }
+        }
+        Ok(WIRQHandlerCollection { handlers })
+    }
+
     pub fn get_weak_handler(
         &mut self,
         irq: u16,
