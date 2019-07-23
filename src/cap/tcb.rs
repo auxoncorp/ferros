fa99bc03e9a9ec668c4ad23351d61773fd4518ff
dan pittman <dan@dpitt.me>
2019-07-23 12:21:40 -0700

    ipc buffers are only optional in tcb configuration

--- src/cap/tcb.rs
+++ src/cap/tcb.rs
@@ -62,7 +62,7 @@ impl LocalCap<ThreadControlBlock> {
         cspace_root: LocalCap<ChildCNode>,
         fault_source: Option<FaultSource<role::Child>>,
         vspace: &VSpace<VSpaceState, role::Local>, // vspace_root,
-        ipc_buffer: LocalCap<Page<page_state::Mapped>>,
+        ipc_buffer: Option<LocalCap<Page<page_state::Mapped>>>,
     ) -> Result<(), SeL4Error> {
         // Set up the cspace's guard to take the part of the cptr that's not
         // used by the radix.
@@ -74,6 +74,12 @@ impl LocalCap<ThreadControlBlock> {
         }
         .words[0] as usize;
 
+        let (buffer_cap, buffer_vaddr) = if let Some(ipc_buffer) = ipc_buffer {
+            (ipc_buffer.cptr, ipc_buffer.vaddr())
+        } else {
+            (seL4_CapNull as usize, 0)
+        };
+
         unsafe {
             seL4_TCB_Configure(
                 self.cptr,
@@ -82,8 +88,8 @@ impl LocalCap<ThreadControlBlock> {
                 cspace_root_data,
                 vspace.root_cptr(),
                 seL4_NilData as usize, // vspace_root_data, always 0, reserved by kernel?
-                ipc_buffer.vaddr(),    // buffer address
-                ipc_buffer.cptr,       // bufferFrame capability
+                buffer_vaddr,          // buffer address
+                buffer_cap,            // bufferFrame capability
             )
         }
         .as_result()
