71bbcee5c55a8899cbfbc733ac28fa8e5ae9391f
Russell Mull <russell@auxon.io>
2019-07-26 13:53:04 -0700

    Use selfe-runtime library

--- example/src/main.rs
+++ example/src/main.rs
@@ -8,7 +8,6 @@ use error::TopLevelError;
 
 fn main() {
     let raw_bootinfo = unsafe { &*sel4_start::BOOTINFO };
-
     run(raw_bootinfo).expect("Failed to run root task setup");
 }
 
@@ -16,13 +15,8 @@ fn run(raw_bootinfo: &'static selfe_sys::seL4_BootInfo) -> Result<(), TopLevelEr
     let (allocator, mut dev_allocator) = micro_alloc::bootstrap_allocators(&raw_bootinfo)?;
     let mut allocator = WUTBuddy::from(allocator);
 
-    debug_println!("hello from the example!");
+    debug_println!("\n\n\n    Hello from the root task!\n\n\n");
 
     Ok(())
 }
 
-
-#[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    sel4_start::debug_panic_handler(&info)
-}
