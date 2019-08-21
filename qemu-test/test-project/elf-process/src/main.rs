ffb7061fd0d932e693da456abe7c409447dbb711
Russell Mull <russell@auxon.io>
2019-08-21 08:52:16 -0700

    Add test for writable globals in elf

--- qemu-test/test-project/elf-process/src/main.rs
+++ qemu-test/test-project/elf-process/src/main.rs
@@ -7,8 +7,15 @@ extern crate selfe_runtime;
 
 use elf_process::ProcParams;
 
+static mut MUT_GLOBAL: u32 = 0;
+
 #[no_mangle]
 pub extern "C" fn _start(params: ProcParams<role::Local>) -> ! {
+    // try to set the mut global, to see that BSS was mapped
+    unsafe {
+        MUT_GLOBAL = 42;
+    }
+
     params
         .outcome_sender
         .blocking_send(&(params.value == 42))
