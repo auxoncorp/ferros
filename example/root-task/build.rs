75344470379cb2c5c511b049c824c817199192b7
Russell Mull <russell@auxon.io>
2019-09-02 18:25:51 -0700

    Upgrade to cargo-5730 v0.2

--- example/root-task/build.rs
+++ example/root-task/build.rs
@@ -1,7 +1,22 @@
-use cargo_5730;
-
+#[cfg(not(workaround_build))]
 fn main() {
-    println!("cargo:rerun-if-changed=build-script/src/main.rs");
-    println!("cargo:rerun-if-changed=build-script/Cargo.toml");
-    cargo_5730::run_build_crate("build-script");
+    cargo_5730::run_build_script();
+}
+
+#[cfg(workaround_build)]
+fn main() {
+    use ferros_build::*;
+    use std::path::Path;
+
+    let out_dir = Path::new(&std::env::var_os("OUT_DIR").unwrap()).to_owned();
+    let bin_dir = out_dir.join("..").join("..").join("..");
+    let resources = out_dir.join("resources.rs");
+
+    let hello = ElfResource {
+        path: bin_dir.join("hello-printer"),
+        image_name: "hello-printer".to_owned(),
+        type_name: "HelloPrinter".to_owned(),
+    };
+
+    embed_resources(&resources, vec![&hello as &dyn Resource]);
 }
--- qemu-test/test-project/root-task/build.rs
+++ qemu-test/test-project/root-task/build.rs
@@ -1,7 +1,33 @@
-use cargo_5730;
-
+#[cfg(not(workaround_build))]
 fn main() {
-    println!("cargo:rerun-if-changed=build-script/src/main.rs");
-    println!("cargo:rerun-if-changed=build-script/Cargo.toml");
-    cargo_5730::run_build_crate("build-script");
+    cargo_5730::run_build_script();
+}
+
+
+#[cfg(workaround_build)]
+fn main() {
+    use ferros_build::*;
+    use std::path::Path;
+    use std::env;
+
+    println!("cargo:rerun-if-env-changed=TEST_CASE");
+
+    let test_case = match env::var("TEST_CASE") {
+        Ok(val) => val,
+        Err(_) => "root_task_runs".to_string(),
+    };
+
+    println!("cargo:rustc-cfg=test_case=\"{}\"", test_case);
+
+    let out_dir = Path::new(&std::env::var_os("OUT_DIR").unwrap()).to_owned();
+    let bin_dir = out_dir.join("..").join("..").join("..");
+    let resources = out_dir.join("resources.rs");
+
+    let elf_proc = ElfResource {
+        path: bin_dir.join("elf-process"),
+        image_name: "elf-process".to_owned(),
+        type_name: "ElfProcess".to_owned(),
+    };
+
+    embed_resources(&resources, vec![&elf_proc as &dyn Resource]);
 }
